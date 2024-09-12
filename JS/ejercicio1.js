db54b7393a778b34b7203470ebeeca8c8ef61460
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-09-12 15:55:23 -0300

    Código completado y comentado

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -1,2 +1,10 @@
 // Función para verificar si el número es par o impar
-let numero = prompt('Ingresa un número', '');
+
+let numero = prompt('Por favor ingresa un número', '');
+
+    // Verificar si el número es par o impar
+    if (numero % 2 === 0) {  //si es resto es igual a 0 se mostrará que el número es par
+        alert("El número " + numero + " es par.");
+    } else {                 // si el resto no es igual a 0 se mostrará que el número es impar
+        alert("El número " + numero + " es impar.");
+    }
