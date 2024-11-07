e5c629b49f3deab821323ea12173b25dee4f5856
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-11-07 00:16:59 -0300

    App Navegación lista

--- React-Native/Navegacion/App.js
+++ React-Native/Navegacion/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={['black', '#d51985']} 
+        colors={['#1c0312', '#e82394']} 
         style={styles.background}
       >
         <Image 
@@ -18,14 +24,20 @@ export default function App() {
         <Text style={styles.text}>Panamera</Text>
         <Text style={styles.subtitulo}>Standly</Text>
 
-        <Ionicons name="heart" size={32} color="red" style={styles.heartIcon} />
+        <TouchableOpacity onPress={toggleLike}>
+          <Ionicons 
+            name={liked ? "heart" : "heart-outline"} 
+            size={32} 
+            color={liked ? "red" : "black"} 
+            style={styles.heartIcon} 
+          />
+        </TouchableOpacity>
 
         <View style={styles.controls}>
           <FontAwesome name="backward" size={32} color="black" />
           <FontAwesome name="play" size={32} color="black" />
           <FontAwesome name="forward" size={32} color="black" />
         </View>
-
       </LinearGradient>
     </View>
   );
@@ -43,10 +55,15 @@ const styles = StyleSheet.create({
   image: {
     width: 250,  
     height: 250, 
-    resizeMode: 'contain', 
+    resizeMode: 'container', 
     marginBottom: 20,
-    borderRadius: 20,
-    marginTop: -80,
+    borderRadius: 15,
+    marginTop: -80,    
+    shadowColor: '#000',          
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+    elevation: 15,  
   },
   text: {
     fontSize: 20,
@@ -62,9 +79,9 @@ const styles = StyleSheet.create({
     marginBottom: 40,
   },
   controls: {
-    flexDirection: 'row',           //Fila horizontal
-    justifyContent: 'space-between', // Espacio entre íconos
-    width: 150,                      
-    marginTop: 35,                   
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 150,
+    marginTop: 35,
   },
 });
