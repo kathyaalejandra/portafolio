8da6cb62ec788ce29da8241cfab3f492d40c1774
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-11-07 00:30:56 -0300

    comenzando ultima app

--- React-Native/AppNavegacion2/App.js
+++ React-Native/AppNavegacion2/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Holaaaaaaaaaaaa!</Text>
       <StatusBar style="auto" />
     </View>
   );
