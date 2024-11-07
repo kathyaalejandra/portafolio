37c420dc56b92a1f22a2debe7ace7810839864e2
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-11-06 21:16:35 -0300

    Probando repositorio otra vez

--- JS/ejercicio1.js
+++ JS/ejercicio1.js
@@ -4,7 +4,7 @@ let numero = prompt('Por favor ingresa un número', '');
 
     // Verificar si el número es par o impar
     if (numero % 2 === 0) {  //si es resto es igual a 0 se mostrará que el número es par
-        alert("El número " + numero + " es par.");
+        alert("El número que ingresaste: " + numero + " es par.");
     } else {                 // si el resto no es igual a 0 se mostrará que el número es impar
-        alert("El número " + numero + " es impar.");
+        alert("El número que ingresaste: " + numero + " es impar.");
     }
