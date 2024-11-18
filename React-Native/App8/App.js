e60e660440cc33b2ddd3c052b8af1eecd1090854
kathyaalejandra <kathyaalejandra.mansilla@alumnos.ulagos.cl>
2024-11-17 23:31:51 -0300

    App8 lista

--- React-Native/App8/App.js
+++ React-Native/App8/App.js
@@ -4,58 +4,82 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        colors={['white', '#22c1c3']} 
-        style={styles.background}
-      >
-        <Image 
-          source={require('./assets/logomuni.png')} 
-          style={styles.image} 
-        />
+    <LinearGradient
+      colors={['#00ffd6', '#376f6a']} 
+      style={styles.gradient}
+    >
+      <View style={styles.container}>
+        {/* Card 1 */}
+        <View style={styles.card}>
+          <Image
+            source={{ uri: 'https://www.grazia.fr/wp-content/uploads/grazia/2023/09/ariana_grande.jpg' }}
+            style={styles.image}
+          />
+          <Text style={styles.title}>Ariana Grande</Text>
+          <Text style={styles.subtitle}>Artista de Pop y R&B</Text>
+          <Text style={styles.description}>
+          Ariana Grande-Butera es una cantante, compositora, actriz y empresaria estadounidense. Comenzó su carrera musical en Broadway a los 15 años en el musical 13.          </Text>
+        </View>
 
-        <Text style={styles.text}>Municipalidad de Quinchao</Text>
-        <Text style={styles.subtitulo}>Departamento de Medio Ambiente</Text>
-
-        <Image 
-          source={{ uri: 'https://onymos.com/wp-content/uploads/2020/10/google-signin-button-1024x260.png' }} 
-          style={styles.imageBottom}
-        />
-      </LinearGradient>
-    </View>
+        {/* Card 2 */}
+        <View style={styles.card}>
+          <Image
+            source={{ uri: 'https://www.classic-music-vault.com/wp-content/uploads/2017/10/maxresdefault-36.jpg' }}
+            style={styles.image}
+          />
+          <Text style={styles.title}>Coldplay</Text>
+          <Text style={styles.subtitle}>Artista de Pop Rock</Text>
+          <Text style={styles.description}>
+          Coldplay es una banda británica de rock alternativo formada en Londres en 1997. Está integrada por Chris Martin, Jonny Buckland, Guy Berryman, Will Champion y Phil Harvey.          </Text>
+        </View>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-  },
-  background: {
-    flex: 1,
+    padding: 30,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 5,
   },
   image: {
-    width: 200,  
-    height: 200, 
-    resizeMode: 'contain', 
-    marginBottom: 20, // Espacio entre imagen y texto muni
+    width: '100%',
+    height: 150,
+    borderRadius: 10,
+    marginBottom: 10,
   },
-  text: {
+  title: {
     fontSize: 20,
-    fontWeight: '600',
-    color: 'black',
-    marginBottom: 5, // Espacio entre textos
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 5,
+    color: '#333',
   },
-  imageBottom: {
-    width: 200,  
-    height: 200, 
-    resizeMode: 'contain', 
-    position: 'absolute', 
-    bottom: 80, 
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 10,
+    textAlign: 'center',
   },
-  subtitulo: {
-    fontSize: 15,
-    color: 'black',
-  }
+  description: {
+    fontSize: 14,
+    color: '#444',
+    lineHeight: 20,
+    textAlign: 'justify'
+  },
 });
